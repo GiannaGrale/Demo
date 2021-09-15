deb2116aa29abedb255625056afd8d5e8ba9c3d2
Anna <giannagrale@gmail.com>
2021-09-15 20:01:54 +0300

    newBrowser

--- src/test/java/tests/CrossBrowserTesting.java
+++ src/test/java/tests/CrossBrowserTesting.java
@@ -7,6 +7,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import steps_Anna.LoginStep;
@@ -29,9 +30,9 @@ public class CrossBrowserTesting extends BaseCross {
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
-            case "Edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
+            case "Opera":
+                WebDriverManager.operadriver().setup();
+                driver = new OperaDriver();
                 break;
             default:
         }
